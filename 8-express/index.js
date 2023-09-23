952516a5a7b59dceeafd727dc2c304f750465446
bhzd <bhzd.bakhshayesh@gmail.com>
2023-09-23 04:57:32 -0700

    express

---
+++ 8-express/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// app.post();
+
+// app.put();
+
+// app.delete();
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});

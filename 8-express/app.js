3e554f00d3a714c5a51db8fd93094ee04ef348a8
bhzd <bhzd.bakhshayesh@gmail.com>
2023-09-23 10:29:30 -0700

    restfull

---
+++ 8-express/app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT ?? 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.get("/api/users", (req, res) => {
+  console.log(req.query);
+  res.send([
+    { id: 1, name: "user1" },
+    { id: 2, name: "user2" },
+    { id: 3, name: "user3" },
+  ]);
+});
+app.get("/api/users/:id", (req, res) => {
+  console.log(req.params);
+  res.send([{ id: req.params.id, name: `user${req.params.id}` }]);
+});
+
+// app.post();
+
+// app.put();
+
+// app.delete();
+
+app.listen(port, () => console.log(`listening on port ${port}`));
--- 8-express/index.js
+++ 8-express/index.js
@@ -1,29 +1,17 @@
-require("dotenv").config();
-
+let users = require("./usersList");
 const express = require("express");
 const app = express();
-const port = process.env.PORT ?? 3000;
+const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
 app.get("/api/users", (req, res) => {
-  console.log(req.query);
-  res.send([
-    { id: 1, name: "user1" },
-    { id: 2, name: "user2" },
-    { id: 3, name: "user3" },
-  ]);
+  res.json({ data: users, message: "ok" });
 });
 app.get("/api/users/:id", (req, res) => {
-  console.log(req.params);
-  res.send([{ id: req.params.id, name: `user${req.params.id}` }]);
+  const user = users.find((user) => user.id === Number(req.params.id));
+  if (!user) {
+    return res.status(404).json({ data: null, message: "User not found" });
+  }
+  res.json({ data: user, message: "ok" });
 });
-
-// app.post();
-
-// app.put();
-
-// app.delete();
 
 app.listen(port, () => console.log(`listening on port ${port}`));

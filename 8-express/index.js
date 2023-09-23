20106efaefea25f2332707882b47cdb00b2f1078
bhzd <bhzd.bakhshayesh@gmail.com>
2023-09-23 11:04:47 -0700

    restfull

--- 8-express/index.js
+++ 8-express/index.js
@@ -2,6 +2,7 @@ let users = require("./usersList");
 const express = require("express");
 const app = express();
 const port = 3000;
+app.use(express.json());
 
 app.get("/api/users", (req, res) => {
   res.json({ data: users, message: "ok" });
@@ -12,6 +13,11 @@ app.get("/api/users/:id", (req, res) => {
     return res.status(404).json({ data: null, message: "User not found" });
   }
   res.json({ data: user, message: "ok" });
+});
+
+app.post("/api/users", (req, res) => {
+  console.log("1:", req.body);
+  res.json({ data: req.body, message: "ok" });
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`));

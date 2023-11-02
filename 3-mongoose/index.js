3d02e00e44d8b857211900e610cfb61cd11b0334
bhzd <bhzd.bakhshayesh@gmail.com>
2023-11-02 14:57:43 +0330

    Comparison Query Operators

--- 3-mongoose/index.js
+++ 3-mongoose/index.js
@@ -1,8 +1,8 @@
 const { connect, Schema, model } = require("mongoose");
 
 connect("mongodb://127.0.0.1:27017/mongoproject")
-  .then(() => console.log("1"))
-  .catch((err) => console.log("2", err));
+  .then(() => console.log("connected"))
+  .catch((err) => console.log("not connected", err));
 
 const userSchema = new Schema({
   fName: String,
@@ -36,3 +36,28 @@ async function getUsers() {
     .count();
 }
 getUsers();
+
+// ==============================================================
+// Comparison Query Operators
+// $eq(==) - $ne(!=)  - $gt(>) - $gte(>=) - $lt(<) - $lte(<=) - $in - $nin
+
+async function getUsers2() {
+  const equalTo27Users = await User.find({ age: { $eq: 27 } }); //age == 27
+  const notEqualTo27Users = await User.find({ age: { $ne: 27 } }); //age != 27
+  const gereterThan27Users = await User.find({ age: { $gt: 27 } }); //age > 27
+  const lessThan27Users = await User.find({ age: { $lt: 27 } }); //age < 27
+  const between227to30Users = await User.find({ age: { $gt: 27, $lte: 30 } }); //age > 27 && age <= 30
+  const only25or30Users = await User.find({ age: { $in: [25, 30] } }); // age == 25 ||  age == 30
+}
+getUsers2();
+
+// ==============================================================
+// or - and
+
+async function getUsers3() {
+  const users1 = await User.find({ fName: "BHZD", admin: true }); // (fName == "BHZD" && admin == true)
+  const users2 = await User.find().and([{ fName: "BHZD" }, { admin: true }]); // (fName == "BHZD" && admin == true)
+
+  const users3 = await User.find().or([{ fName: "BHZD" }, { admin: true }]); // (fName == "BHZD" || admin == true)
+}
+getUsers3();
